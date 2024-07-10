97aee823eb18e04ddce226b61a79f67639e9f23d
asamamun <asamamun@gmail.com>
2024-07-10 13:29:20 +0600

    env added

--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,5 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+    // Route::post('/logout', [AuthController::class, 'logout']);
 });
