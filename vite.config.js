d393163ccb96ff03cdea74b28fcd89bbba02fec3
asamamun <asamamun@gmail.com>
2024-07-13 12:51:57 +0600

    vite loadEnv fx used

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,35 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
     plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-        
-    ],
-    base: '/Laravel11-Vue3-sanctum/public/build/',
-    define: {
-        'process.env': {},
-      },
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
+      laravel({
+        input: [
+          'resources/sass/app.scss',
+          'resources/js/app.js',
+        ],
+        refresh: true,
+      }),
+      vue({
+        template: {
+          transformAssetUrls: {
+            base: null,
+            includeAbsolute: false,
+          },
         },
+      }),
+    ],
+    base: env.APP_ROOT + '/build/',
+    define: {
+      'process.env': env
     },
+    resolve: {
+      alias: {
+        vue: 'vue/dist/vue.esm-bundler.js',
+      },
+    },
+  }
 });
